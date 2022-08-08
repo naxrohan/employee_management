357e5040968d6ccda89ade24f301c854835f4abe
naxrohan <rohan.jd@hotmail.com>
2022-08-08 11:45:29 +0100

    emp ctrl & route created

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\UserController;
+use App\Http\Controllers\EmployeeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('users', UserController::class);
+Route::resource('employee', EmployeeController::class);
