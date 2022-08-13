78a44f0db8c33099b321c1551b9977cc330e2bc8
naxrohan <rohan.jd@hotmail.com>
2022-08-13 20:17:30 +0100

    created job & batch

---
+++ app/Models/JobBatch.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JobBatch extends Model
+{
+    use HasFactory;
+    
+    protected $table = 'job_batches';
+    
+    protected $fillable = [
+        
+    ];
+}
